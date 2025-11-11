36736d60b81bb3bf45708a1e2db5fb9898665b7c
Huynh Tuong <lehuynhtuong13@gmail.com>
2025-11-11 08:34:07 +0700

    change vietnamese -> english

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -73,7 +73,7 @@ const Header: React.FC = () => {
                         <Link to="/">HOME</Link>
                         <Link to="/about">ABOUT</Link>
                         <Link to="/contact">CONTACT</Link>
-                        <Link to="/booking">Booking TABLE</Link>
+                        <Link to="/booking">BOOKING TABLE</Link>
                     </nav>
 
                     {/* Bên phải */}
